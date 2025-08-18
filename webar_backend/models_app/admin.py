2c02d6ee07e19fd19b4f4bbd0a907043067cce38
Ng Tiin Xiang <txng@ttvision.local>
2025-08-18 14:59:23 +0800

    i dont know, nothing yet

--- webar_backend/models_app/admin.py
+++
@@ -1,7 +0,0 @@
-# models_app/admin.py
-from django.contrib import admin
-from .models import ThreeDModel
-
-@admin.register(ThreeDModel)
-class ThreeDModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'file')  # Show name + file in the list view
